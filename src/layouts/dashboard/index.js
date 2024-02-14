e847cdc4332e5e4db1783ebe20097635a0aea45a
Riyaansh-Mittal <riyaanshmittal14@gmail.com>
2024-02-14 12:13:42 +0530

    make sidebar with MUI Stack, box and theme. Also understand suspense, lazy and custom hooks

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -2,11 +2,119 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
-
   return (
     <>
-      Dashboard Layout
+      <Box
+        p={2}
+        sx={{
+          backgroundColor: theme.palette.paper,
+          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          height: "100vh",
+          width: 100,
+        }}
+      >
+        <Stack
+          direction="column"
+          alignItems={"center"}
+          sx={{ height: "100%" }}
+          spacing={3}
+          justifyContent={"space-between"}
+        >
+          <Stack alignItems={"center"} spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5,
+                // for borderRadius: 1.5*8 = 12
+              }}
+            >
+              <img src={Logo} alt="App Logo" />
+            </Box>
+            <Stack
+              sx={{ width: "max-content" }}
+              direction="column"
+              alignItems="center"
+              spacing={3}
+            >
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "#fff" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    sx={{
+                      width: "max-content",
+                      color:
+                        theme.palette.mode === "light"
+                          ? "#000"
+                          : theme.palette.text.primary,
+                    }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
+              <Divider sx={{ width: "48px" }} />
+              {selected === 3 ? (
+                <Box
+                  p={1}
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                    <Gear />
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    setSelected(3);
+                  }}
+                  sx={{
+                    width: "max-content",
+                    color:
+                      theme.palette.mode === "light"
+                        ? "#000"
+                        : theme.palette.text.primary,
+                  }}
+                >
+                  <Gear />
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+          <Stack spacing={4}>
+            <AntSwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
+            <Avatar src={faker.image.avatar} />
+          </Stack>
+        </Stack>
+      </Box>
       <Outlet />
+      {/*Outlet is used so that the generalApp code can also be rendered*/}
     </>
   );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -20,12 +20,18 @@ export default function Router() {
   return useRoutes([
     {
       path: "/",
+      // for this path, '/', DashboardLayout shows us. It won't show without this path.
       element: <DashboardLayout />,
+      //all children are wrapped inside DashboardLayout
       children: [
+        //here for '/', we navigate to /app by DEFAULT_PATH
         { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
+        //if user goes to '/app', then GeneralApp shows up
         { path: "app", element: <GeneralApp /> },
-        
+        //Here, we have set up fallbacks
+        // for '/404'
         { path: "404", element: <Page404 /> },
+        //Navigate to /404
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
     },
@@ -34,6 +40,7 @@ export default function Router() {
 }
 
 const GeneralApp = Loadable(
-  lazy(() => import("../pages/dashboard/GeneralApp")),
+  // to show loading... for the time the page takes to load -> Suspense and Lazy
+  lazy(() => import("../pages/dashboard/GeneralApp"))
 );
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
