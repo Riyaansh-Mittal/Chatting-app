968b49af1314cca0e28229a07e332211b2941d87
Riyaansh-Mittal <riyaanshmittal14@gmail.com>
2024-04-16 22:16:27 +0530

    Added snackbar

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "../../utils/axios";
+import { showSnackbar } from "./app";
 
 const initialState = {
   isLoggedIn: false,
@@ -59,9 +60,12 @@ export function LoginUser(formValues) {
             token: response.data.token,
           })
         );
+
+        dispatch(showSnackbar({severity: "success", message: response.data.message}))
       })
       .catch(function (error) {
         console.log(error);
+        dispatch(showSnackbar({severity: "error", message: error.message}))
       });
   };
 }
